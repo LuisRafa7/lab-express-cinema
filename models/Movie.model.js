0c280aec7f7fab7e9ec141c2d898f6251d39a73d
Luis Almeida Henriques <luisrafa7@gmail.com>
2023-10-20 16:18:57 +0200

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const movieSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    director: {
+      type: String,
+    },
+    stars: {
+      type: [String],
+    },
+    image: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    showtimes: {
+      type: [String],
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Movies = model("Movies", movieSchema);
+
+module.exports = Movies;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movies = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) =>
+  Movies.find()
+    .then((response) => res.render("movies", { movies: response }))
+    .catch((err) => console.log(err))
+);
+
+router.get("/movies/:id", (req, res) => {
+  const idMovie = req.params.id;
+  Movies.findById(idMovie)
+    .then((response) => res.render("singleMovie", { movie: response }))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
